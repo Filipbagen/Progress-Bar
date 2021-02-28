977aa2e2943a82f69b9dfc6858b168e36b59b379
Filipbagen <filip.bagen@gmail.com>
2021-02-28 18:13:31 +0100

    Updated css
    Compleatly new design

--- progress.js
+++ progress.js
@@ -9,19 +9,26 @@ const getProgress = () => {
 
   // Calculate the difference between the dates
   const totDays = (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)
-  console.log('Total days in university: ' + totDays)
 
   // Calculate compleated days
-  const compleated = ((new Date().getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)).toFixed(1)
-  console.log('Days compleated: ' + compleated)
+  const compleated = Math.floor(((new Date().getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)))
 
   // Calculate the days left
-  const daysLeft = totDays - compleated
+  const daysLeft = Math.floor(totDays - compleated)
+
+  // Calculate the years left
+  const yearsLeft = (daysLeft / 365).toFixed(1)
 
   // Calculate percentage compleated
   const percentageDiff = ((compleated / totDays).toFixed(2)) * 100
 
-  const percent = document.querySelector('.progressBar').style.width = percentageDiff + '%'
-  document.querySelector('.text').innerHTML = 'Du har gått ' + percent + ' av universitetet. Heja heja'
-  document.querySelector('.text2').innerHTML = 'Du har ' + daysLeft + ' dagar kvar vilket är ' + (daysLeft / 365).toFixed(1) + ' år'
+  document.querySelector('#current').style.left = 'calc(' + percentageDiff + '%' + ' - 2pt)'
+  document.querySelector('#foundationYear').style.left = '16.67%'
+  document.querySelector('#abroad').style.left = '66.67%'
+
+  document.querySelector('#percent').innerHTML = percentageDiff + '%'
+  document.querySelector('#percent').style.left = 'calc(' + percentageDiff + '%' + ' - 11pt)'
+
+  document.querySelector('.progressBar').style.width = percentageDiff + '%'
+  document.querySelector('.subtitle').innerHTML = 'Du har klarat ' + compleated + ' dagar och har ' + yearsLeft + ' år kvar!'
 }
